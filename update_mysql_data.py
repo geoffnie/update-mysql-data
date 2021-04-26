c0385e6b56016127c7c15d4d5d8eb0b10aa2cf0b
geoffnie <geoffnie@163.com>
2021-04-26 17:06:56 +0800

    更新批量excel中sheet，更新字段取消合并及填充功能，增加第一个条件字段为空过滤跳过，执行下一条功能

--- update_mysql_data.py
+++ update_mysql_data.py
@@ -85,7 +85,16 @@ def update_mysql_data(df, user ,password ,host ,port ,database, update_table , u
     cursor =conn.cursor()
     for i in range(0,len(df)):
 #    for i in range(0,5):
+#        for m in range(0,len(update_condition_fields)):
+        
+        #如果本地文件条件字段文件中为空【第一个条件字段】，则退出，执行下一条更新语句
+        if df[file_conditon_fields[0]].iloc[i]=='':
+            continue
+            
+        #更新set语句段
         update_set_sements = ' , '.join([update_fields[a] + ' = ' + '''"{0}"'''.format(df[file_fields[a]].iloc[i]) for a in range(0,len(update_fields))])
+        
+        #更新条件语句段
         update_where_sements = ' and ' + ' and '.join([update_condition_fields[a] + ' = ' + '''"{0}"'''.format(df[file_conditon_fields[a]].iloc[i]) for a in range(0,len(update_condition_fields))]) + extral_condition
         sql_execute = '''UPDATE {0} SET {1} WHERE 1 = 1 {2}  ;'''.format(update_table, update_set_sements, update_where_sements)
         print(sql_execute)
@@ -110,6 +119,7 @@ update_condition_fields = get_config('mysql', 'update_condition_fields').split("
 file_fields = get_config('mysql', 'file_fields').split(",")
 file_conditon_fields = get_config('mysql', 'file_conditon_fields').split(",")
 extral_condition = get_config('mysql', 'extral_condition')
+excel_combine_rows = get_config('mysql', 'excel_combine_rows').split(",")
 
 print(host)
 print(password)
@@ -140,18 +150,66 @@ else:
         for file in file_list:
             if '.csv' in file:
                 df = pd.read_csv(r"{0}/{1}".format(root,file)).replace(np.nan, '', regex=True)
-            elif '.xlsx' in file:    
-                df = pd.read_excel(r"{0}/{1}".format(root,file), sheet_name="Sheet1").replace(np.nan, '', regex=True)
+                for file_field in file_fields + file_conditon_fields:
+                    df['{0}'.format(file_field)] = [x.replace(' ','') for x in df['{0}'.format(file_field)]]
+                    df['{0}'.format(file_field)] = [x.replace('\r\n','') for x in df['{0}'.format(file_field)]]
+                    df['{0}'.format(file_field)] = [x.replace('\r','') for x in df['{0}'.format(file_field)]]
+                    df['{0}'.format(file_field)] = [x.replace('\n','') for x in df['{0}'.format(file_field)]]
+                    df['{0}'.format(file_field)] = [x.replace('\\r\\n','') for x in df['{0}'.format(file_field)]]
+                update_mysql_data(df, user ,password ,host ,port ,database, update_table , update_fields, update_condition_fields , file_fields , file_conditon_fields , extral_condition)
+                print(file + '已更新：' + str(len(df)) + "条记录")
             
-            for file_field in file_fields + file_conditon_fields:
-                print()
-                df['{0}'.format(file_field)] = [x.replace(' ','') for x in df['{0}'.format(file_field)]]
-                df['{0}'.format(file_field)] = [x.replace('\r\n','') for x in df['{0}'.format(file_field)]]
-                df['{0}'.format(file_field)] = [x.replace('\r','') for x in df['{0}'.format(file_field)]]
-                df['{0}'.format(file_field)] = [x.replace('\n','') for x in df['{0}'.format(file_field)]]
-                df['{0}'.format(file_field)] = [x.replace('\\r\\n','') for x in df['{0}'.format(file_field)]]
-            update_mysql_data(df, user ,password ,host ,port ,database, update_table , update_fields, update_condition_fields , file_fields , file_conditon_fields , extral_condition)
-            print(file + '已更新：' + str(len(df)) + "条记录")
+            elif '.xlsx' in file:   
+                xls = pd.ExcelFile(r"{0}/{1}".format(root,file))
+                sheet_names = xls.sheet_names   
+                for sheet in sheet_names:
+                    print("{0}/{1}".format(root,file) + "-------" + sheet)
+                    df = pd.read_excel(r"{0}/{1}".format(root,file), sheet_name=sheet).replace(np.nan, '', regex=True)
+                    
+                    #定义空值上一值和定义新列表
+                    up_one=''
+                    list_new = []
+                    
+                    #对每一个excel列存在合并单元格的情况进行处理
+                    for excel_combine_row in excel_combine_rows:
+                        for value in df['{0}'.format(excel_combine_row)].tolist():
+                            
+                            if value == '':
+                                list_new.append(up_one)
+                            else:
+                                up_one = value
+                                list_new.append(value)
+                        df['{0}'.format(excel_combine_row)] = list_new
+                        
+                    #对所有条件及更新字段进行去除空格和换行操作
+                    for file_field in file_fields + file_conditon_fields + excel_combine_rows:
+                        df['{0}'.format(file_field)] = [x.replace(' ','') for x in df['{0}'.format(file_field)]]
+                        df['{0}'.format(file_field)] = [x.replace('\r\n','') for x in df['{0}'.format(file_field)]]
+                        df['{0}'.format(file_field)] = [x.replace('\r','') for x in df['{0}'.format(file_field)]]
+                        df['{0}'.format(file_field)] = [x.replace('\n','') for x in df['{0}'.format(file_field)]]
+                        df['{0}'.format(file_field)] = [x.replace('\\r\\n','') for x in df['{0}'.format(file_field)]]
+                    
+                    #更新数据到数据库中
+                    update_mysql_data(df, user ,password ,host ,port ,database, update_table , update_fields, update_condition_fields , file_fields , file_conditon_fields , extral_condition)
+                    print(file + '已更新：' + str(len(df)) + "条记录")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
